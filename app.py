b58200a6e00bcb921bdbf384d099c36376b0e0cd
shany-shan <117829439+shany-shan@users.noreply.github.com>
2023-08-12 11:14:20 +0530

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import secrets
 import os
 from flask import Flask
 from routes import pages
@@ -5,9 +6,12 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
+secret_key = secrets.token_hex(32)
+
 def create_app():
     app=Flask(__name__)
     app.register_blueprint(pages)
+    app.secret_key = secret_key
     client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.habittracker
 
