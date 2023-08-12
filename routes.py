e2fe50d86e0cefe278d56d45c25e3df4d3a9a221
shany-shan <117829439+shany-shan@users.noreply.github.com>
2023-08-12 11:15:44 +0530

    routes.py

--- routes.py
+++ routes.py
@@ -1,12 +1,50 @@
 import uuid
-from flask import Blueprint,current_app,render_template,request,url_for,redirect#importing bluerint instead of flask
+from flask import Blueprint,session,current_app,flash,render_template,request,url_for,redirect#importing bluerint instead of flask
 import datetime
+from passlib.hash import pbkdf2_sha256
+
 
 
 #replace all app to pages and everywhere in url use habits. both in html and routesfile
 pages=Blueprint("habits",__name__,static_folder="static",template_folder="templates")#instead of ap=flask ,pages=blueprint
 
 
+    
+@pages.route("/",methods=["GET","POST"])
+def reg():
+    if request.method =="POST":
+            email=request.form.get("email")
+            password=request.form.get("password")
+            hash_pwd= pbkdf2_sha256.hash(password)
+
+            current_app.db.user.insert_one({
+                            "email":email,"password":hash_pwd})
+            flash("Successfully signed up.")
+            return redirect(url_for('habits.log')) 
+    return render_template("reg.html")
+
+
+
+@pages.route("/login",methods=["GET","POST"])
+def log():
+        if request.method =="POST":
+            email=request.form.get("email")
+            password=request.form.get("password")
+            
+            user=current_app.db.user.find_one({"email":email})
+            if user and pbkdf2_sha256.verify(password, user["password"]):
+                session["email"] = user["email"]
+                return redirect(url_for('habits.home'))
+            else:
+                flash("Incorrect Email/Password !!!")
+                return redirect(url_for('habits.log'))    
+        return render_template("log.html")
+
+@pages.route("/logout")
+def logout():
+   session.clear()
+   return redirect(url_for("habits.log")) 
+     
 
 #this functn gives 3days before and after selected date.
 # to make this functn available to all rendering temp we use contextprocessor.
@@ -23,7 +61,7 @@ def today_at_midnight():
     return datetime.datetime(today.year, today.month, today.day)#we need only date
 
 
-@pages.route("/")
+@pages.route("/home")
 def home():
     date_str=request.args.get("date")#this is the selected date we get from endpoint
     if date_str:
@@ -31,10 +69,12 @@ def home():
     else:
         selected_date=today_at_midnight()
 
-    habits_on_date = current_app.db.habits.find({"added": {"$lte": selected_date}})
+    user_id = session.get("email")
+
+    habits_on_date = current_app.db.habits.find({"added": {"$lte": selected_date},"user_id":user_id})
     completions = [
         habit["habit"]
-        for habit in current_app.db.completions.find({"date": selected_date})
+        for habit in current_app.db.completions.find({"date": selected_date,"user_id": user_id})
     ]
  
     return render_template("index.html",
@@ -48,26 +88,30 @@ def home():
 def add_habits():
     today=today_at_midnight()
 
+    user_id = session.get("email")
+
     if request.method == "POST":
         current_app.db.habits.insert_one(
-            {"_id": uuid.uuid4().hex, "added": today, "name": request.form.get("habit")}
+            {"_id": uuid.uuid4().hex,
+              "added": today,
+             "name": request.form.get("habit"),
+            "user_id": user_id}
         )
- 
         return redirect(url_for("habits.home")) #this will redirect to homepage when add btn is clicked
     return render_template("add_habits.html",
                 selected_date=today#becoz newhabits r added only on todays date not on selected date 
                            )
 
-
 @pages.route("/complete", methods=["POST"])
 def complete():
+    user_id = session.get("email")
+
     date_string = request.form.get("date")#data we get from hidden fields
     date = datetime.datetime.fromisoformat(date_string)
     habit = request.form.get("habitId")
-    current_app.db.completions.insert_one({"date": date, "habit": habit})
+    current_app.db.completions.insert_one({"date": date, "habit": habit,"user_id":user_id})
 
     return redirect(url_for("habits.home", date=date_string))
-
 
 @pages.route("/delete/<habit_id>",methods=["POST"])
 def delete(habit_id):
